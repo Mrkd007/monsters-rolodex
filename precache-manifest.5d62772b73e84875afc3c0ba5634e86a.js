c6b1adb59258c071d369ec7ef59fff0e1eeeec14
Mrkd007 <itskd17@gmail.com>
2020-03-19 09:24:39 +0530

    Updates

---
+++ precache-manifest.5d62772b73e84875afc3c0ba5634e86a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c06a2a98e7cc045556b24106fa5ae5e3",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "7838a3c55fadb1a84be6",
+    "url": "/monsters-rolodex/static/css/main.08c2fe0b.chunk.css"
+  },
+  {
+    "revision": "30436d15d072549122d4",
+    "url": "/monsters-rolodex/static/js/2.0d232ef2.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/monsters-rolodex/static/js/2.0d232ef2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7838a3c55fadb1a84be6",
+    "url": "/monsters-rolodex/static/js/main.dabe7c4f.chunk.js"
+  },
+  {
+    "revision": "9e014af7b12dc10ef867",
+    "url": "/monsters-rolodex/static/js/runtime-main.ff038b7c.js"
+  }
+]);
